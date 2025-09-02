a3cc28424869ec8bc8920fb6522d6c63c6bc66fb
gbrielbram18 <biel300816@gmail.com>
2025-09-02 15:03:57 -0300

    adicionando novas atividades de junção de HTML com JavaScript

---
+++ index-js/js/calc.js
@@ -0,0 +1,31 @@
+/*
+
+crie calculos 
+
+*/
+
+//Variaveis
+let A = 10 ;
+let B = 20 ;
+let C = 30 ;
+let D = 40 ;
+
+//Opeções 
+let adicicao = A + B;
+let sub = A - B;
+let divisao = A / B;
+let mult = A * B;
+
+//Imprimir as repostas
+console.log(`A soma do valor ${A} + valor ${B} e igual á: ${adicicao}`);
+console.log(`A subtração do valor ${A} + valor ${B} e igual á: ${sub}`);
+console.log(`A divisão do valor ${A} + valor ${B} e igual á: ${divisao} `);
+console.log(`A multiplicação do valor ${A} + valor ${B} e igual á: ${mult} `);
+
+
+//calculo de 1° grau
+let calcum = A + B * C / D;
+console.log(" ");
+console.log( "O resultado do calculo de 1° grau foi: ",calcum)
+
+
