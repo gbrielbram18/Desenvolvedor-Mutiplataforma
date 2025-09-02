27675c3e0a95c907d827b2f0825d1e07259e79c7
gbrielbram18 <biel300816@gmail.com>
2025-09-02 16:22:26 -0300

    adiciomnando atividadede button com js

--- index-js/js/calc.js
+++ index-js/js/calc.js
@@ -1,31 +1,29 @@
-/*
-
-crie calculos 
-
-*/
 
 //Variaveis
-let A = 10 ;
-let B = 20 ;
-let C = 30 ;
-let D = 40 ;
+let num1 = 10; 
+let num2 = 20;
 
-//Opeções 
-let adicicao = A + B;
-let sub = A - B;
-let divisao = A / B;
-let mult = A * B;
+//Operações
+let soma = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
 
-//Imprimir as repostas
-console.log(`A soma do valor ${A} + valor ${B} e igual á: ${adicicao}`);
-console.log(`A subtração do valor ${A} + valor ${B} e igual á: ${sub}`);
-console.log(`A divisão do valor ${A} + valor ${B} e igual á: ${divisao} `);
-console.log(`A multiplicação do valor ${A} + valor ${B} e igual á: ${mult} `);
+// funções
+function somar (){
+    alert("a soma do calculo foi: " + soma);
+    
+}
 
+function subitracao() {
+    alert("a subtração do calculo foi: " + sub);
+}
 
-//calculo de 1° grau
-let calcum = A + B * C / D;
-console.log(" ");
-console.log( "O resultado do calculo de 1° grau foi: ",calcum)
+function multiplicacao(){
+    alert("a multiplicação do calculo foi: " + mult);
 
+}
 
+function divisao(){
+    alert("a divisao do calculo foi: " + div);
+}
