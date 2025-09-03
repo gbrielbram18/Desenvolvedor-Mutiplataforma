394e5bb14e2e6084c8feed9e0eecded141b76b94
gbrielbram18 <biel300816@gmail.com>
2025-09-03 15:00:45 -0300

    adicionando nova funtion de revelar a idade

---
+++ index-js/js/calcNex.js
@@ -0,0 +1,24 @@
+
+
+function mostrarNome(nome,idade){
+    console.log(nome);
+
+}
+function idade(){
+
+}
+
+
+function pularlinha(){
+    console.log("<br><br>");
+}
+
+// chamar função
+mostrarNome("gabriel");
+pularlinha();
+mostrarNome("ashaganda");
+pularlinha();
+mostrarNome("ronaldo");
+
+
+
