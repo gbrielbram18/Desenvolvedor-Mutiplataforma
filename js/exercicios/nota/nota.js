5bdec261ae067443b386769eac3b99eabcd694e2
gbrielbram18 <biel300816@gmail.com>
2025-09-10 16:10:05 -0300

    adicionando alterações no js e o css

--- js/exercicios/nota/nota.js
+++ js/exercicios/nota/nota.js
@@ -1,6 +1,28 @@
 const inputNota = document.getElementById("nota");
 const resultado = document.getElementById("result");
 
-function resultadoNotaClick(event){
-    alert("reultado")
+function resultadoNotaClick(event) {
+
+    event.preventDefault();
+    // console.log(inputNota);
+
+    if (inputNota.value >= 6) {
+       alert("voce esta aprovado ");
+       resultado.innerHTML= "Aprovado";
+
+    } else if (inputNota.value <= 5) {
+      resultado.innerHTML= "reporvado, estude mais";
+
+    } else if (inputNota.value == 10) {
+       resultado.innerHTML= "Aprovado";
+    }
+
+    //Validação de fomrulario
+
+    if (inputNota.value == "") {
+        alert("preencha o campo")
+    }
+
+    inputNota.value = "";
+
 }
